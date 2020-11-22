9f64abcfac4c34adfab3e3b3a2558a1827f36886
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-22 17:10:26 +0100

    creating functionality for new task

--- app/src/main/java/com/example/projectboard/AddNewTask.java
+++ app/src/main/java/com/example/projectboard/AddNewTask.java
@@ -1,5 +1,10 @@
 package com.example.projectboard;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,5 +18,28 @@ public class AddNewTask extends BottomSheetDialogFragment {
     private EditText newTaskText;
     private Button newTaskSaveButton;
     private DatabaseHandler db;
+
+    public static AddNewTask newInstance() {
+        return new AddNewTask();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setStyle(STYLE_NORMAL, R.style.DialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_task, container, false);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        return view;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        newTaskText = getView().findViewById(R.id.newTaskText);
+    }
 
 }
