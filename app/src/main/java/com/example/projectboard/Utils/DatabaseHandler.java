8a3fa45720aead28290f67decaea4ae296bdabf8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-22 14:28:05 +0100

    creating database

--- app/src/main/java/com/example/projectboard/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/projectboard/Utils/DatabaseHandler.java
@@ -1,5 +1,7 @@
 package com.example.projectboard.Utils;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -10,6 +12,28 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String ID = "id";
     private static final String TASK = "task";
     private static final String STATUS = "status";
-    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + "INTEGER PRIMARY KEY AUTOINCREMENT"
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                                                   + TASK + "TEXT, " +STATUS + " INTEGER)";
+
+    private SQLiteDatabase db;
+
+    private DatabaseHandler(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TODO_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop the older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
+        // CREATE TABLE AGAIN
+        onCreate(db);
+    }
+
+    public void openDataBase() {
+        db = this.getWritableDatabase();
+    }
 }
