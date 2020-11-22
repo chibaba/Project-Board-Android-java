2ca6b8353cb43d3833519fcf6fd6898794e70103
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-22 04:39:58 +0100

    todoAdapter

--- app/src/main/java/com/example/projectboard/MainActivity.java
+++ app/src/main/java/com/example/projectboard/MainActivity.java
@@ -6,9 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.projectboard.Adapter.TodoAdapter;
+import com.example.projectboard.Model.TodoBModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView  tasksRecyclerView;
+    private TodoAdapter   tasksAdapter;
+
+    private List<TodoBModel>  taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
 
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        tasksAdapter = new TodoAdapter(this);
+
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        TodoBModel task = new TodoBModel();
+        task.setTask("This is a sample project");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
 
 
     }
