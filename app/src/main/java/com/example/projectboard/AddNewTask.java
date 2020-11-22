60e8fa3d0d0487faabf9ffe104c3c5bee34f3a5e
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-22 16:35:34 +0100

    add new task

---
+++ app/src/main/java/com/example/projectboard/AddNewTask.java
@@ -0,0 +1,17 @@
+package com.example.projectboard;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.projectboard.Utils.DatabaseHandler;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddNewTask extends BottomSheetDialogFragment {
+
+    public static final String TAG = "ActionBottomDialog";
+
+    private EditText newTaskText;
+    private Button newTaskSaveButton;
+    private DatabaseHandler db;
+
+}
