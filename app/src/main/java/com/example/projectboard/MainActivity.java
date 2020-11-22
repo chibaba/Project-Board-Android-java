e5bfea67e3d9f4086ef5764804070e56502970be
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-22 04:24:27 +0100

    todoAdapter

--- app/src/main/java/com/example/projectboard/MainActivity.java
+++ app/src/main/java/com/example/projectboard/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.projectboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView  tasksRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
--- app/src/main/java/com/example/projectboard/SplashActivity.java
+++ app/src/main/java/com/example/projectboard/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,5 +15,12 @@ public class SplashActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
     }
 }
